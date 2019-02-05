38142aa26a69c9b2e20bd3708cfafc145f7ca127
Steven Rausch <stevenrausch44@gmail.com>
2019-02-05 15:51:46 -0600

    hello world

--- HelpImHungry/Controllers/HomeController.cs
+++ HelpImHungry/Controllers/HomeController.cs
@@ -9,30 +9,14 @@ using HelpImHungry.Models;
 namespace HelpImHungry.Controllers
 {
     public class HomeController : Controller
-    {
+    {[HttpGet]
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
