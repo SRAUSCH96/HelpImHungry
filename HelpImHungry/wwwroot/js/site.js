a08f2926e72dfd70d453c44eebae98e74b02851f
Steven Rausch <stevenrausch44@gmail.com>
2019-03-11 00:24:51 -0500

    front end work

--- HelpImHungry/wwwroot/js/site.js
+++ HelpImHungry/wwwroot/js/site.js
@@ -4,4 +4,25 @@
 // Write your JavaScript code.
 var visibleDiv = 0;
 
-function 
+function showDiv() {
+    $(".restaurant").hide();
+    $(".restaurant1").hide();
+    $(".restaurant:eq(" + visibleDiv + ")").show();
+}
+
+function showNext() {
+    if (visibleDiv == $(".restaurant").length - 1) {
+        visibleDiv = 0;
+    } else {
+        visibleDiv++;
+    }
+    showDiv();
+}
+function showPrev() {
+    if (visibleDiv == 0) {
+        visibleDiv = $(".restaurant").length - 1;
+    } else {
+        visibleDiv--;
+    }
+    showDiv();
+}
